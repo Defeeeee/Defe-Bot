899867168b9651c10c2a44a83fb35c5d13be7da4
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-07 00:04:09 -0300

    Recruit, Tempmod, Unrecruit,Verify

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -1,4 +1,6 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+
 
 module.exports = class TestCommand extends BaseCommand {
   constructor() {
@@ -6,6 +8,10 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    message.channel.send('si');
+    const VerifyEmbed = new Discord.MessageEmbed()
+      .setTitle(`Bienvenido a ${message.guild.name}`)
+      .setDescription('Para acceder a todos los mensajes escribi `e!verify`')
+      .setFooter('Defe Bot desarrollado por Federico Diaz Nemeth')
+    message.channel.send(VerifyEmbed)
   }
 }

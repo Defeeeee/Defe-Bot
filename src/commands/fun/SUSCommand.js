ee6c052d7e48bc9a7357b359fcbe40d886b96bb0
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-28 22:44:24 -0300

    Create SUSCommand.js

---
+++ src/commands/fun/SUSCommand.js
@@ -0,0 +1,27 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const discord = require('discord.js');
+
+module.exports = class SUSCommand extends BaseCommand {
+  constructor() {
+    super('SUS', 'fun', []);
+  }
+
+  async run(client, message, args) {
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('No tenes permisos para usar ese comando')
+    
+    const SUSRole = message.guild.roles.cache.get('837139025646714883');
+    const modrole = message.guild.roles.cache.get('733344436611973220');
+    const masterrole = message.guild.roles.cache.get('733344476034236516');
+    const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    const SUSEmbed = new discord.MessageEmbed()
+      .setTitle(`${mentionedMember} IS KINDA SUS`)
+      .setDescription('sus')
+      .setColor('RANDOM');
+
+    await mentionedMember.roles.add(SUSRole).catch(err => console.log(err));
+      await mentionedMember.roles.remove(modrole).catch(err => console.log(err));
+      await mentionedMember.roles.remove(masterrole).catch(err => console.log(err));
+    
+    message.channel.send(SUSEmbed);
+  }
+}

94bdf69bb6ab70347166a89440f61ba080caaa99
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-30 12:59:56 -0300

    Update A350Command.js

--- src/commands/fun/A350Command.js
+++ src/commands/fun/A350Command.js
@@ -1,11 +1,17 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
 
 module.exports = class A350Command extends BaseCommand {
   constructor() {
     super('a350', 'fun', []);
   }
 
-  run(client, message, args) {
-    message.channel.send('a350 command works');
+  async run(client, message, args) {
+    const airbembed = new Discord.MessageEmbed()
+      .setTitle('a350 bois')
+      .setImage('https://cdn.discordapp.com/attachments/795095607915053136/837719645603627048/2018_Airbus_A350-1000_Cooling.jpg')
+      .setColor('RANDOM')
+    
+    message.channel.send(airbembed);
   }
 }
